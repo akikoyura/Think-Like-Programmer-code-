dec4ab44a4a28667b8985a712187049577f9c24a
akiko yura <akikoyura@akikos-mac.local>
2018-05-26 18:36:43 -0700

    Initial Commit

---
+++ Pure Puzzle_ex2.1.cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pure Puzzle_ex2.1.cpp
+//
+//  Created by akiko yura on 5/26/18.
+//  Copyright © 2018 akiko yura. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
