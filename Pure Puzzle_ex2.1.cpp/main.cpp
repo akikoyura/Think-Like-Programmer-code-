98d9941c250ef73b468cb4d019fd225b0d242ece
akiko yura <akikoyura@akikos-Mac.local>
2018-05-26 22:57:58 -0700

    done ex2.1

--- Pure Puzzle_ex2.1.cpp/main.cpp
+++ Pure Puzzle_ex2.1.cpp/main.cpp
@@ -6,10 +6,26 @@
 //  Copyright © 2018 akiko yura. All rights reserved.
 //
 
+//Think like programmer chapter 2. - exercises 2.1
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n;
+    cout<<"moi nhap n: "<<endl;
+    cin>>n;
+    for(int row = n ;row>0;row--)
+    {
+        
+        for(int space=0;space<=n-row;space++)
+        {
+            cout<<(space==0?"":" ");
+        }
+        for(int hashnum=0;hashnum<=2*row;hashnum++)
+        {
+            cout<<(hashnum==2*row?"\n":"#");
+        }
+    }
+    
     return 0;
 }
+
